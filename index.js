f07bb932a34bc3e634496d8ee8a3e7b7346637bc
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-02-27 16:29:35 +0000

    format code in index.js

--- index.js
+++ index.js
@@ -87,75 +87,90 @@ const nextEmployeePrompt = () => {
 };
 
 const addEngineer = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Specify the engineer's name:",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "What is the engineer's ID?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "What is the engineer's email address?",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "Please specify their GitHub username:",
-            name: "github",
-        }
-    ]).then(response => {
-        // Create instance of Engineer using the user's response
-        const engineer = new Engineer(response.name, response.id, response.email, response.github);
-        // Push new Engineer to members array
-        members.push(engineer);
-        nextEmployeePrompt(); 
-    })
-}
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Specify the engineer's name:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the engineer's ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the engineer's email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Please specify their GitHub username:",
+        name: "github",
+      },
+    ])
+    .then((response) => {
+      // Create instance of Engineer using the user's response
+      const engineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      // Push new Engineer to members array
+      members.push(engineer);
+      nextEmployeePrompt();
+    });
+};
 
 const addIntern = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Specify the intern's name:",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "What is the intern's ID?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "What is the intern's email address?",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "Please specify the school they go to:",
-            name: "school",
-        }
-    ]).then(response => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Specify the intern's name:",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the intern's ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the intern's email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Please specify the school they go to:",
+        name: "school",
+      },
+    ])
+    .then((response) => {
       // Create new instance of Intern via the user response
-        const intern = new Intern(response.name, response.id, response.email, response.school);
-        // Push intern onto members array
-        members.push(intern);
-        nextEmployeePrompt();
-    })
-}
+      const intern = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+      // Push intern onto members array
+      members.push(intern);
+      nextEmployeePrompt();
+    });
+};
 const createTeamHTML = () => {
-    // Use render function (from page-template.js) to add team members from 'members' array to document
-    const HTML = render(members);
-    // If output directory does not exist, create one
-    if (!fs.existsSync(OUTPUT_DIR)) {
-        fs.mkdirSync(OUTPUT_DIR);
-    }
-    // Create new HTML file using the file system node package 
-    fs.writeFileSync(outputPath, HTML, 'utf-8')
-    console.log("An HTML document has been dynamically generated in the 'output' directory!")
-
-}
+  // Use render function (from page-template.js) to add team members from 'members' array to document
+  const HTML = render(members);
+  // If output directory does not exist, create one
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  // Create new HTML file using the file system node package
+  fs.writeFileSync(outputPath, HTML, "utf-8");
+  console.log(
+    "An HTML document has been dynamically generated in the 'output' directory!"
+  );
+};
